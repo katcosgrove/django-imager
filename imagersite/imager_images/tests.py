71713edd553cb0913d67bd55f9e1917bb62bfb66
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-25 14:22:51 -0700

    starting writing tests

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import User, Album, Photo
+import factory
 
 # Create your tests here.
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+
+
+class ImageFactory(factor)
