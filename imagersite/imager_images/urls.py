cfd76f76f6fde921c3b5479c2f8754f176c26761
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-04-26 22:51:36 -0700

    route change, image size tweak

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -3,7 +3,7 @@ from .views import library_view, photos_view, albums_view, photo_view, album_vie
 
 
 urlpatterns = [
-    path('', library_view, name='library'),
+    path('library/', library_view, name='library'),
     path('photos/', photos_view, name='photos'),
     path('photos/<str:photo_id>', photo_view, name='photo'),
     path('albums/', albums_view, name='albums'),
