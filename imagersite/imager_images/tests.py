5b573b87427d1e0a1e552ba15a002a2a579f3cc9
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-30 20:12:46 -0700

    up to 65 tests, cleaned a bunch of useless code out

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -15,7 +15,6 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         """Meta class for a user."""
-
         model = User
 
     username = factory.Faker('user_name')
@@ -24,10 +23,9 @@ class UserFactory(factory.django.DjangoModelFactory):
 
 class PhotoFactory(factory.django.DjangoModelFactory):
     """Create instances of Photos."""
-
+    
     class Meta:
         """Meta class for a photo."""
-
         model = Photo
 
     image = SimpleUploadedFile('test.jpg', b'file_content', content_type='image/jpg')
@@ -48,7 +46,6 @@ class AlbumFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         """Meta class for an album."""
-
         model = Album
 
     title = fake.text(170)
@@ -67,120 +64,100 @@ class PhotoAlbumUnitTest(TestCase):
     """Test Album functionality and interactivity."""
 
     @classmethod
-    def setUpUser(self):
-        """Create a single album instance."""
+    def setUp(self):
+        """Setup user, album, and photo instances for testing"""
         super(TestCase, self)
-        user = User.build(username='watdude',
-                          email='watdude@wat.up')
-        user.set_password('helloworld')
-        user.user_id = 1
+        user = User(username='testing',
+                    email='testing@testing.com')
         user.save()
         self.user = user
+        self.client = Client()
 
-    @classmethod
-    def setUpAlbum(self):
-        """Create a single album instance."""
-        super(TestCase, self)
+        photo = PhotoFactory.build()
+        photo.user_id = self.user.id
+        photo.save()
+        self.photo = photo
+
         album = AlbumFactory.build()
-        album.user = self.user
+        album.user_id = self.user.id
         album.save()
         self.album = album
 
     @classmethod
-    def setUpPhoto(self):
-        """Create a single photo instance."""
-        super(TestCase, self)
-        photo = PhotoFactory.build()
-        photo.user = self.user
-        photo.save()
-        self.photo = photo
-
-    @classmethod
-    def tearDownPhotoAlbums(self):
-        """Destroy photo album instance."""
-        super(TestCase, self)
-        Album.objects.all().delete()
+    def tearDown(self):
+        """Destroy user and related instance."""
         User.objects.all().delete()
         Photo.objects.all().delete()
+        super(TestCase, self)
 
     def test_album_is_being_created(self):
         """Test if an album is created."""
-        one_album = Album
-        self.assertIsNotNone(one_album)
+        self.assertIsNotNone(self.album)
 
     def test_album_has_a_user(self):
         """Test if album has a user."""
-        one_album = Album
-        one_album.user = User
-        self.assertIsNotNone(one_album.user)
-        self.assertEqual(one_album.user, User)
+        self.assertIsNotNone(self.album.user)
+        self.assertEqual(self.album.user, self.user)
 
     def test_album_has_a_title(self):
         """Test if an album has a title."""
-        one_album = Album
-        self.assertTrue(one_album.title)
+        self.assertTrue(self.album.title)
 
     def test_album_has_a_description(self):
         """Test if an album has a description."""
-        one_album = Album
-        self.assertTrue(one_album.description)
+        self.assertTrue(self.album.description)
 
     def test_photo_is_being_created(self):
         """Test if a photo is created."""
-        one_photo = Photo
-        self.assertIsNotNone(one_photo)
+        self.assertIsNotNone(self.photo)
 
     def test_photo_has_a_user(self):
         """Test photo belongs to a user."""
-        one_photo = Photo
-        one_photo.user = User
-        self.assertIsNotNone(one_photo.user)
-        self.assertEqual(one_photo.user, User)
+        self.assertIsNotNone(self.photo.user)
+        self.assertEqual(self.photo.user, self.user)
 
     def test_photo_has_an_image(self):
         """Test if a photo has an image."""
-        one_photo = Photo
-        self.assertIsNotNone(one_photo.image)
+        self.assertIsNotNone(self.photo.image)
 
     def test_photo_has_a_title(self):
         """Test if a photo has a title."""
-        one_photo = Photo
-        self.assertIsNotNone(one_photo.title)
+        self.assertIsNotNone(self.photo.title)
 
     def test_photo_added_to_album(self):
         """Test a photo is successfully added to an album."""
-        one_photo = Photo
-        one_album = Album
-        one_photo.album = one_album
-        self.assertTrue(one_photo.album, one_album)
-        self.assertTrue(one_album.__sizeof__, 1)
+        self.photo.album = self.album
+        self.assertTrue(self.photo.album, self.album)
+        self.assertTrue(self.album.__sizeof__, 1)
 
     def test_multiple_photos_added_to_album(self):
         """Test a multiple photos can be added to a single album."""
         one_photo = Photo
         two_photo = Photo
         three_photo = Photo
-        one_album = Album
-        one_photo.album = one_album
-        two_photo.album = one_album
-        three_photo.album = one_album
-        self.assertTrue(one_photo.album, one_album)
-        self.assertTrue(two_photo.album, one_album)
-        self.assertTrue(three_photo.album, one_album)
-        self.assertTrue(one_album.__sizeof__, 3)
+
+        one_photo.album = self.album
+        two_photo.album = self.album
+        three_photo.album = self.album
+
+        self.assertTrue(one_photo.album, self.album)
+        self.assertTrue(two_photo.album, self.album)
+        self.assertTrue(three_photo.album, self.album)
+        self.assertTrue(self.album.__sizeof__, 3)
 
     def test_one_photo_added_to_multiple_albums(self):
         """Test the same photos can be added to multiple albums."""
         one_album = Album
         two_album = Album
         three_album = Album
-        one_photo = Photo
-        one_album.photo = one_photo
-        two_album.photo = one_photo
-        three_album.photo = one_photo
-        self.assertIs(one_photo, one_album.photo)
-        self.assertIs(one_album.photo, three_album.photo)
-        self.assertIs(three_album.photo, two_album.photo)
+
+        one_album.photo = self.photo
+        two_album.photo = self.photo
+        three_album.photo = self.photo
+
+        self.assertIs(self.photo, one_album.photo)
+        self.assertIs(self.photo, two_album.photo)
+        self.assertIs(self.photo, three_album.photo)
 
 
 class TestImagesViews(TestCase):
